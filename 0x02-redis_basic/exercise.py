666ccdebcf68408a66e1baf16716597139384991
Ali-TBB <Ali-Tbb@git.com>
2024-08-14 12:23:05 +0100

    task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ Redis """
-from typing import Union
+from typing import Callable, Optional, Union
 import redis
 import uuid
 
@@ -34,3 +34,37 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+    
+    def get(self, key: str, fn: Optional[Callable] = None):
+            """
+            Retrieves the value associated with the given key from Redis.
+
+            Args:
+                key (str): The key to retrieve the value for.
+                fn (Optional[Callable]): An optional function to apply to the retrieved value.
+
+            Returns:
+                The retrieved value, or None if the key does not exist.
+
+            """
+            data = self._redis.get(key)
+            
+            if not data:
+                return None
+            if fn is int:
+                return self.get_int(data)
+            if fn is str:
+                return self.get_str(data)
+            if Callable(fn):
+                return fn(data)
+            return data
+
+    def get_str(self, data: bytes) -> str:
+        """ Converts bytes to string
+        """
+        return data.decode('utf-8')
+
+    def get_int(self, data: bytes) -> int:
+        """ Converts bytes to integers
+        """
+        return int(data)
